4bba52af272a63ee5ecc4a3e47468a895f41d2b2
Baorlong11 <baolongkg2020@gmail.com>
2026-05-09 13:23:37 +0700

    'TH'

---
+++ Buoi6/src/Cummunityuni/Executive.java
@@ -0,0 +1,5 @@
+package Cummunityuni;
+
+public class Executive {
+
+}
---
+++ Buoi6/src/Cummunityuni/MoHinhLop.java
@@ -0,0 +1,19 @@
+package Cummunityuni;
+
+public abstract class MoHinhLop {
+    protected String name;
+    protected String address;
+    protected String phone;
+
+    public MoHinhLop(String name, String address, String phone) 
+    {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+    public String toString() 
+    {
+        return "Name: " + name + "\nAddress: " + address + "\nPhone: " + phone;
+    }
+    public abstract double pay();
+}
