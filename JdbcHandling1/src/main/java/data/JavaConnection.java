e50b682416bd0a1c653a0a52fb024c99ad1c33c0
Baorlong11 <baolongkg2020@gmail.com>
2026-05-27 16:20:43 +0700

    TH

---
+++ JdbcHandling1/src/main/java/data/JavaConnection.java
@@ -0,0 +1,21 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JavaConnection {
+	private static final String URL = "jdbc:mysql://localhost:3306/quanlygiaovien";
+	private static final String USER = "root";
+	private static final String PASSWORD = "";
+	
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			return DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (Exception e) {
+			System.out.println("Loi ket noi database!");
+            e.printStackTrace();
+            return null;
+		}
+	}
+}
